a50da9139f1c4b23f5d741907fcad09e306b49eb
zekunjin <zekun.jin@foxmail.com>
2024-01-30 16:12:46 +0800

    style: update popup layout

--- src/components/UUatchLogo.vue
+++ src/components/UUatchLogo.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+defineProps<{ disabled?: boolean }>()
+</script>
+
 <template>
-  <img src="/assets/icon-128.png" block alt="extension icon">
+  <img src="/assets/icon-128.png" block :class="{ 'saturate-50': disabled }" alt="extension icon">
 </template>
--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,14 +1,29 @@
 <script setup lang="ts">
+import { version } from '../../package.json'
+
 const { isAvailable } = useStore()
 
 const toggleAvailable = () => { isAvailable.value = !isAvailable.value }
 </script>
 
 <template>
-  <main w="300px" px-4 py-5 text-gray-700>
-    <nav flex items-center gap-2 cursor-pointer @click="toggleAvailable">
-      <UUatchLogo w-4 />
-      <span font-bold>UUatch</span>
+  <main w="300px" py-2 text-gray-700>
+    <nav
+      px-4
+      py-3
+      flex
+      items-center
+      justify-between
+      w-full
+      cursor-pointer
+      hover:bg-gray-100
+      @click="toggleAvailable"
+    >
+      <div flex items-center gap-2>
+        <UUatchLogo w-4 :disabled="!isAvailable" />
+        <span font-bold select-none :class="{ 'text-gray-300': !isAvailable }">UUatch</span>
+      </div>
+      <span scale-75>v{{ version }}</span>
     </nav>
   </main>
 </template>
