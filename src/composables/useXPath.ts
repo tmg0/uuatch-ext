7c6844a0114e84655349192c79d926703ca16efb
zekunjin <zekun.jin@foxmail.com>
2024-01-30 12:07:12 +0800

    chore: use custom pkg

--- src/composables/useXPath.ts
+++ src/composables/useXPath.ts
@@ -1,5 +1,49 @@
 import { type MaybeRef } from "@vueuse/core"
-import getXPath from 'get-xpath'
+
+export type Options = {
+  ignoreId: boolean
+};
+
+const defaultOptions: Options = {
+  ignoreId: false
+};
+
+export default function getXPath( el: any, customOptions?: Partial< Options > ): string {
+  const options = { ...defaultOptions, ...customOptions };
+  let nodeElem = el;
+  if ( nodeElem && nodeElem.id && ! options.ignoreId ) {
+      return "//*[@id=\"" + nodeElem.id + "\"]";
+  }
+  let parts: string[] = [];
+  while ( nodeElem && Node.ELEMENT_NODE === nodeElem.nodeType ) {
+      let nbOfPreviousSiblings = 0;
+      let hasNextSiblings = false;
+      let sibling = nodeElem.previousSibling;
+      while ( sibling ) {
+          if ( sibling.nodeType !== Node.DOCUMENT_TYPE_NODE &&
+              sibling.nodeName === nodeElem.nodeName
+          ) {
+              nbOfPreviousSiblings++;
+          }
+          sibling = sibling.previousSibling;
+      }
+      sibling = nodeElem.nextSibling;
+      while ( sibling ) {
+          if ( sibling.nodeName === nodeElem.nodeName ) {
+              hasNextSiblings = true;
+              break;
+          }
+          sibling = sibling.nextSibling;
+      }
+      let prefix = nodeElem.prefix ? nodeElem.prefix + ":" : "";
+      let nth = nbOfPreviousSiblings || hasNextSiblings
+          ? "[" + ( nbOfPreviousSiblings + 1 ) + "]"
+          : "";
+      parts.push( prefix + nodeElem.localName + nth );
+      nodeElem = nodeElem.parentNode;
+  }
+  return parts.length ? "/" + parts.reverse().join( "/" ) : "";
+}
 
 export const useXPath = (element: MaybeRef<HTMLElement | null>) => {
   const xpath = computed(() => getXPath(unref(element)))
