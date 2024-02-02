597a1cd803d4053bae702565c3875a9fea26002f
zekunjin <zekun.jin@foxmail.com>
2024-02-02 10:55:25 +0800

    chore: use dynamic message

--- shim.d.ts
+++ shim.d.ts
@@ -5,6 +5,5 @@ declare module 'webext-bridge' {
     // define message protocol types
     // see https://github.com/antfu/webext-bridge#type-safe-protocols
     'tab-prev': { title: string | undefined }
-    'get-current-tab': ProtocolWithReturn<{ tabId: number }, { title?: string }>
   }
 }
